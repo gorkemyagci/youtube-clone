850ac0cb80bb8fa7281b12ef9166be3ffc0424b2
gorkemyagci <gorkemyagci705@gmail.com>
2025-02-22 04:07:24 +0300

    youtube-clone

--- modules/studio/ui/sections/form-section.tsx
+++ modules/studio/ui/sections/form-section.tsx
@@ -196,7 +196,7 @@ const FormSectionSuspense = ({ videoId }: FormSectionProps) => {
                             <p className="text-sm text-muted-foreground">Manage your video details</p>
                         </div>
                         <div className="flex items-center gap-x-2">
-                            <Button type="submit" disabled={update.isPending}>
+                            <Button type="submit" disabled={update.isPending || !form.formState.isDirty}>
                                 Save
                             </Button>
                             <DropdownMenu>
